7468136c8d69959b7eac1dca01135a5b01eaeff7
nishavaswani10 <vaswani.nisha@gmail.com>
2018-06-24 22:33:07 +0100

    Update MainApplication.java

--- src/main/java/com/example/demo/main/MainApplication.java
+++ src/main/java/com/example/demo/main/MainApplication.java
@@ -1,12 +1,10 @@
 package com.example.demo.main;
 
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.Scanner;
 import java.util.logging.Logger;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.demo.config.Config;
 
@@ -14,35 +12,28 @@ import com.example.demo.config.Config;
  * @author Nisha.Vaswani
  *
  */
-@SpringBootApplication
+
 public class MainApplication {
 	static Logger LOG = Logger.getLogger("DemoApplication.Class");
-	
-	public static String overrides;
-	
+
 	public static String fileName;
-	
-	public static void main(String[] args) throws FileNotFoundException {
-		
-		SpringApplication.run(MainApplication.class, args);
+
+	public static void main(String[] args) throws IOException {
+		Properties prop = new Properties();
 		Config config=new Config();
+		String overrides;
+		InputStream inputFile = MainApplication.class.getClassLoader().getResourceAsStream("application.properties");
+		prop.load(inputFile);
 		Scanner scanner=new Scanner(System.in);
 		System.out.println("Please enter the property you want to read ");
 		String input=scanner.next();
-		Config.load(fileName,overrides.split(","));
-		
+		overrides=prop.getProperty("value.overrides");
+		System.out.println(overrides);
+		Config.load(prop.getProperty("file.path"),overrides.split(","));
 		String output=config.get(input);
 		LOG.info("Output: "+output);
 	}
-	
-	
-	@Value("${value.overrides}")
-    public void setOverride(String overrideValue) {
-      overrides = overrideValue;
-    }
-	
-	@Value("${file.path}")
-    public void setFilePath(String filePath) {
-      fileName = filePath;
-    }
+
+
+
 }
